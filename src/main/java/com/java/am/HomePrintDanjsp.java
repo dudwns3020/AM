2b06515ddf0622b71c386d316fabd563fbec6e1b
dudwns3020 <dudwns3020@naver.com>
2021-09-27 00:00:24 +0900

    구구단서블릿

---
+++ src/main/java/com/java/am/HomePrintDanjsp.java
@@ -0,0 +1,21 @@
+package com.java.am;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// /home/printDan
+@WebServlet("/home/printDan2")
+public class HomePrintDan2Ser extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.getRequestDispatcher("/jsp/home/printDan.jsp").forward(request, response);
+	}
+
+}
